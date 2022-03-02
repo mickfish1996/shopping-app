0ece1f001208303401b4e0237fd66c187a390bab
mickfish96 <mickfish1996@gmail.com>
2022-03-02 11:21:56 -0700

    added the Shopping list class

---
+++ app/src/main/java/com/example/shoppinglist/ShoppingList.java
@@ -0,0 +1,23 @@
+package com.example.shoppinglist;
+
+import java.util.HashMap;
+
+public class ShoppingList {
+    HashMap<String, Integer> shoppingList = new HashMap<String, Integer>();
+
+    public static void main(String[] args){
+
+    }
+
+    public ShoppingList(){
+
+    }
+
+    public void addToList(String item, int num){
+        shoppingList.put(item, num);
+    }
+
+    public HashMap<String, Integer> getList(){
+        return shoppingList;
+    }
+}
